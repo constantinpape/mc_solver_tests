75c04507af568566c9d94b505a1b248b6e4e3f5a
Constantin Pape <c.pape@gmx.net>
2017-04-28 22:52:17 +0200

    converter to opengm models

--- convert_to_opengm_model.py
+++ convert_to_opengm_model.py
@@ -1,9 +1,12 @@
 from  utils import write_to_opengm, read_from_mcppl
 import vigra
+import os
 
 def convert_to_opengm(sample):
     uv_path   = './models/sample%s_uvs.h5' % sample
-    cost_path = './models/sample%s_uvs.costs' % sample
+    cost_path = './models/sample%s_costs.h5' % sample
+    assert os.path.exists(uv_path), uv_path
+    assert os.path.exists(cost_path), cost_path
 
     n_var, uvs, costs = read_from_mcppl(uv_path, cost_path)
     write_to_opengm(n_var, uvs, costs, './models/sample%s_opengm.gm')
