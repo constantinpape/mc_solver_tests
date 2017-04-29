f11e3fd91521a2953467f248a5fc9613232e7402
Constantin Pape <c.pape@gmx.net>
2017-04-29 20:37:23 +0200

    Plotting

--- tests/anytime_plots.py
+++ tests/anytime_plots.py
@@ -5,7 +5,7 @@ import vigra
 
 def plot_energies_sample(times, energies, labels, title):
     assert len(times) == len(energies)
-    assert len(times) == len(labels)
+    assert len(times) == len(labels), "%i, %i" % (len(times), len(labels))
     f, ax = plt.subplots()
     for i, t in enumerate(times):
         en = energies[i]
@@ -17,16 +17,57 @@ def plot_energies_sample(times, energies, labels, title):
     plt.show()
 
 
+# Evaluation showed that pybindings and commandline yield the same results
+def mcmp_py_vs_cmd():
+    for sample in ('sampleA','sampleB','sampleC'):
+        times = [
+                 vigra.readHDF5(   './anytime_data/%s_mcmp_py.h5'% sample, 'rt_primal'),
+                 vigra.readHDF5(   './anytime_data/%s_mcmp_py.h5'% sample, 'rt_dual'),
+                 vigra.readHDF5(   './anytime_data/%s_mcmp_cmd.h5'% sample, 'rt_primal'),
+                 vigra.readHDF5(   './anytime_data/%s_mcmp_cmd.h5'% sample, 'rt_dual')
+                ]
+        energies = [
+                    vigra.readHDF5('./anytime_data/%s_mcmp_py.h5'% sample, 'primal'),
+                    vigra.readHDF5('./anytime_data/%s_mcmp_py.h5'% sample, 'dual'),
+                    vigra.readHDF5('./anytime_data/%s_mcmp_cmd.h5'% sample, 'primal'),
+                    vigra.readHDF5('./anytime_data/%s_mcmp_cmd.h5'% sample, 'dual')
+                  ]
+        plot_energies_sample(
+                times,
+                energies,
+                ['mcmp_py-primal', 'mcmp_py-dual', 'mcmp_py-primal', 'mcmp_py-dual'],
+                sample)
+
+
+def nifty_vs_mcmp(with_dual = False, with_ilp = False):
+    for sample in ('sampleA','sampleB','sampleC'):
+        times = [
+                vigra.readHDF5(   './anytime_data/%s_fm.h5' % sample,  'run_times'),
+                vigra.readHDF5(   './anytime_data/%s_mcmp_py.h5'% sample, 'rt_primal')
+        ]
+        energies = [
+                vigra.readHDF5('./anytime_data/%s_fm.h5'% sample,   'energies'),
+                vigra.readHDF5('./anytime_data/%s_mcmp_py.h5'% sample, 'primal')
+        ]
+        labels = ['fm', 'mcmp-primal']
+
+        if with_dual:
+            times.append( vigra.readHDF5(   './anytime_data/%s_mcmp_py.h5'% sample, 'rt_dual') )
+            energies.append( vigra.readHDF5('./anytime_data/%s_mcmp_py.h5'% sample, 'dual'))
+            labels.append('mcmp-dual')
+
+
+        if with_ilp:
+            times.append(vigra.readHDF5('./anytime_data/%s_ilp.h5'% sample, 'run_times'))
+            energies.append(vigra.readHDF5('./anytime_data/%s_ilp.h5'% sample, 'energies'))
+            labels.append('ilp')
+
+        plot_energies_sample(
+                times,
+                energies,
+                labels,
+                sample)
+
+
 if __name__ == '__main__':
-    sample = 'sampleA'
-    times = [vigra.readHDF5(   './anytime_data/%s_fm.h5' % sample,   'run_times'),
-             vigra.readHDF5(   './anytime_data/%s_mcmp.h5'% sample, 'rt_primal'),
-             vigra.readHDF5(   './anytime_data/%s_mcmp.h5'% sample, 'rt_dual')]
-    energies = [vigra.readHDF5('./anytime_data/%s_fm.h5'% sample,   'energies'),
-                vigra.readHDF5('./anytime_data/%s_mcmp.h5'% sample, 'primal'),
-                vigra.readHDF5('./anytime_data/%s_mcmp.h5'% sample, 'dual')]
-    plot_energies_sample(
-            times,
-            energies,
-            ['fm', 'mcmp-primal', 'mcmp-dual'],
-            sample)
+    nifty_vs_mcmp(with_dual = True)
