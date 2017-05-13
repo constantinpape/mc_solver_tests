7873dfa986d9ceaa32b64011e39f91284ff073b0
Constantin Pape <c.pape@gmx.net>
2017-05-13 14:36:27 +0200

    offsets in block exp

--- tests/sampleD_blocking.py
+++ tests/sampleD_blocking.py
@@ -1,4 +1,5 @@
 import nifty
+import time
 import numpy as np
 import sys
 sys.path.append('..')
@@ -7,6 +8,10 @@ from utils import read_from_mcluigi, read_nodes, model_paths_mcluigi
 
 models = ['sampleD_full', 'sampleD_L1', 'sampleD_L2', 'sampleD_L3', 'sampleD_L4']
 
+time_offsets_mc    = np.array([0, 1484.5, 298.3, 349.2, 1035.2])
+time_offsets_merge = np.array([0, 3286.5, 356.6, 255.1, 198.2])
+t_offsets          = np.cumsum(time_offsets_mc + time_offsets_merge)[::-1]
+
 
 def run_sample_d(model, time_limit, time_offset, global_obj = None):
     n_var, uv_ids, costs = read_from_mcluigi(model_paths_mcluigi[model])
@@ -14,7 +19,9 @@ def run_sample_d(model, time_limit, time_offset, global_obj = None):
 
     # TODO select solver
 
+    t_inf  = time.time()
     node_res = run_nifty_solver(obj, factory, verbose = True, time_limit = time_limit + time_offset )
+    t_inf = time.time() - t_inf
 
     if global_obj is not None:
         to_global_nodes = read_nodes(model_paths_mcluigi[model], dtype = 'uint32')
@@ -22,9 +29,21 @@ def run_sample_d(model, time_limit, time_offset, global_obj = None):
         for node_id, cluster_id in enumerate(node_res):
             global_res[to_global_nodes[node_id]] = cluster_id
 
-        return global_obj.evalNodeLabels(gloabl_res)
+        return global_obj.evalNodeLabels(gloabl_res), t_inf
     else:
-        return obj.evalNodeLabels(node_res)
+        return obj.evalNodeLabels(node_res), t_inf
+
+
+def test_full(time_limit):
+    t_off = t_offsets[0]
+    e_full, t_full = run_sample_d(models[0], time_limit, t_off)
+
+
+def test_blocking():
+    pass
+
 
 if __name__ == '__main__':
-    run_sample_d()
+    # 5 hour time limit
+    time_limit = 5 * 3600
+    #run_sample_d()
