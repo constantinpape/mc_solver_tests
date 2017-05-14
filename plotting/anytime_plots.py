8980edae0c07629f7be7401d6986c3774e8c402b
Constantin Pape <c.pape@gmx.net>
2017-05-14 20:01:49 +0200

    more plotting

--- plotting/anytime_plots.py
+++ plotting/anytime_plots.py
@@ -1,93 +1,24 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
 import cPickle as pickle
 import vigra
+import sys
+sys.path.append('..')
+from utils import *
 
-def parse_out_niftyfm(out):
-    out = out.split('\n')
-
-    energies  = []
-    run_times = []
-    iter_without_improvement = []
-
-    for line in out[1:]:
-        if not line.startswith('Energy:'):
-            if line.startswith('end inference'):
-                break
-            else:
-                continue
-        values = line.split()
-        energies.append(float(values[1]))
-        run_times.append(int(values[3]))
-        iter_without_improvement.append( int(float(values[5])) )
-
-    return energies, run_times, iter_without_improvement
+parser = {
+        'ilp' : parse_out_niftyilp,
+        'fm-ilp' : parse_out_niftyfm,
+        'fm-kl' : parse_out_niftyfm,
+        'fm-greedy' : parse_out_niftyfm,
+        'mp' : parse_out_mcmp,
+        'mp-fmkl' : parse_out_mcmp,
+        'mp-fmgreedy' : parse_out_mcmp,
+        }
 
 
-def parse_out_niftyilp(out):
-    out = out.split('\n')
-
-    energies  = []
-    run_times = []
-    n_violated = []
-
-    for line in out[1:]:
-        if not line.startswith('Energy:'):
-            if line.startswith('end inference'):
-                break
-            else:
-                continue
-        values = line.split()
-        energies.append(float(values[1]))
-        run_times.append(int(values[3]))
-        n_violated.append( int(float(values[5])) )
-
-    return energies, run_times, n_violated
-
-
-def parse_out_mcmp(out):
-    out = out.split('\n')
-
-    primal    = []
-    dual      = []
-    rt_primal = []
-    rt_dual   = []
-    dual_improvements = []
-
-    for line in out:
-        if line.startswith('best triplet'):
-            line = line.split()
-            dual_improvements.append(line[-1])
-
-        if not line.startswith('iteration'):
-            continue
-
-        line = line.split()
-        iter_num    = int(''.join(c for c in line[2] if c.isdigit()))
-        lower_bound = float(''.join(c for c in line[6] if c.isdigit() or c == '-'))
-        run_time   = float(''.join(c for c in line[-1] if c.isdigit() or c == '.'))
-
-        rt_dual.append(run_time)
-        dual.append(lower_bound)
-
-        if line[7] == 'upper':
-            val = line[10] if not line[10] == 'inf,' else '0'
-            primal.append(
-                float(''.join(c for c in val if c.isdigit() or c == '-'))
-            )
-            rt_primal.append(run_time)
-
-    # postprocess the primals for a better plot and drop primals that have 0 value
-    primal = np.array(primal)
-    rt_primal = np.array(rt_primal)
-    mask = primal != 0
-    primal = primal[mask]
-    rt_primal = rt_primal[mask]
-
-    return rt_primal, primal, rt_dual, dual, dual_improvements
-
-
-def plot_energies_sample(times, energies, labels, title):
+def plot_energies_sample(times, energies, labels, title, bounds = None):
     assert len(times) == len(energies)
     assert len(times) == len(labels), "%i, %i" % (len(times), len(labels))
     f, ax = plt.subplots()
@@ -97,87 +28,51 @@ def plot_energies_sample(times, energies, labels, title):
     ax.set_xlabel('runtimes [s]')
     ax.set_ylabel('energy')
     ax.set_title('title')
+    if bounds is not None:
+        ax.set_ylim(bounds)
     ax.legend()
     plt.show()
 
 
-# Evaluation showed that pybindings and commandline yield the same results
-def mcmp_py_vs_cmd():
-    for sample in ('sampleA','sampleB','sampleC'):
-        times = [
-                 vigra.readHDF5(   './anytime_data/%s_mcmp_py.h5'% sample, 'rt_primal'),
-                 vigra.readHDF5(   './anytime_data/%s_mcmp_py.h5'% sample, 'rt_dual'),
-                 vigra.readHDF5(   './anytime_data/%s_mcmp_cmd.h5'% sample, 'rt_primal'),
-                 vigra.readHDF5(   './anytime_data/%s_mcmp_cmd.h5'% sample, 'rt_dual')
-                ]
-        energies = [
-                    vigra.readHDF5('./anytime_data/%s_mcmp_py.h5'% sample, 'primal'),
-                    vigra.readHDF5('./anytime_data/%s_mcmp_py.h5'% sample, 'dual'),
-                    vigra.readHDF5('./anytime_data/%s_mcmp_cmd.h5'% sample, 'primal'),
-                    vigra.readHDF5('./anytime_data/%s_mcmp_cmd.h5'% sample, 'dual')
-                  ]
-        plot_energies_sample(
-                times,
-                energies,
-                ['mcmp_py-primal', 'mcmp_py-dual', 'mcmp_py-primal', 'mcmp_py-dual'],
-                sample)
-
-
-def nifty_vs_mcmp(with_dual = False, with_ilp = False):
-    for sample in ('sampleA','sampleB','sampleC'):
-        times = [
-                vigra.readHDF5(   './anytime_data/%s_fm.h5' % sample,  'run_times'),
-                vigra.readHDF5(   './anytime_data/%s_mcmp_py.h5'% sample, 'rt_primal')
-        ]
-        energies = [
-                vigra.readHDF5('./anytime_data/%s_fm.h5'% sample,   'energies'),
-                vigra.readHDF5('./anytime_data/%s_mcmp_py.h5'% sample, 'primal')
-        ]
-        labels = ['fm', 'mcmp-primal']
-
-        if with_dual:
-            times.append( vigra.readHDF5(   './anytime_data/%s_mcmp_py.h5'% sample, 'rt_dual') )
-            energies.append( vigra.readHDF5('./anytime_data/%s_mcmp_py.h5'% sample, 'dual'))
-            labels.append('mcmp-dual')
-
-
-        if with_ilp:
-            times.append(vigra.readHDF5('./anytime_data/%s_ilp.h5'% sample, 'run_times'))
-            energies.append(vigra.readHDF5('./anytime_data/%s_ilp.h5'% sample, 'energies'))
-            labels.append('ilp')
-
-        plot_energies_sample(
-                times,
-                energies,
-                labels,
-                sample)
-
 
 def sampleD_plots(with_dual = False):
-    for sample in ('sampleD_medium', 'sampleD_large'):
-        times = [
-                #vigra.readHDF5(   './anytime_data/%s_fm.h5' % sample,  'run_times'),
-                vigra.readHDF5(   './anytime_data/%s_mcmp_py.h5'% sample, 'rt_primal')
-        ]
-        energies = [
-                #vigra.readHDF5('./anytime_data/%s_fm.h5'% sample,   'energies'),
-                vigra.readHDF5('./anytime_data/%s_mcmp_py.h5'% sample, 'primal')
-        ]
-        labels = [
-                #'fm',
-                'mcmp-primal'
-                ]
 
-        if with_dual:
-            times.append( vigra.readHDF5(   './anytime_data/%s_mcmp_py.h5'% sample, 'rt_dual') )
-            energies.append( vigra.readHDF5('./anytime_data/%s_mcmp_py.h5'% sample, 'dual'))
-            labels.append('mcmp-dual')
+    sample = 'sampleD_sub_L1'
+    #solvers = ['ilp', 'fm-ilp', 'fm-kl', 'mp', 'mp-fmgreedy']
+    solvers = ['ilp', 'fm-ilp', 'fm-kl']
 
-        plot_energies_sample(
-                times,
-                energies,
-                labels,
-                sample)
+    #if with_dual:
+    #    times.append( vigra.readHDF5(   './anytime_data/%s_mcmp_py.h5'% sample, 'rt_dual') )
+    #    energies.append( vigra.readHDF5('./anytime_data/%s_mcmp_py.h5'% sample, 'dual'))
+    #    labels.append('mcmp-dual')
+
+    times = []
+    energies = []
+    labels = []
+    bounds = []
+    for solver in solvers:
+        out_path = '../tests/anytime_data/sampleD_sub/%s_%s.txt' % (sample, solver)
+        t, e = parser[solver](out_path)
+        assert len(t) == len(e)
+        times.append(t)
+        energies.append(e)
+        labels.append(solver)
+        if solver in 'ilp':
+            bounds.append( ( np.min(e), np.min(e) ) )
+        if solver in ('fm-ilp', 'fm-kl'):
+            bounds.append( ( np.min(e),np.max(e) ) )
+
+    bounds = np.array(bounds)
+    bounds = [bounds[:,0].min(), round(bounds[:,1].max(),-2)]
+    print bounds
+
+    plot_energies_sample(
+            times,
+            energies,
+            labels,
+            sample,
+            bounds
+            )
 
 
 if __name__ == '__main__':
---
+++ utils/parse_cmd_output.py
@@ -0,0 +1,108 @@
+import cPickle as pickle
+import numpy as np
+
+
+def parse_out_niftyfm(out_path, with_iter = False):
+
+    with open(out_path) as f:
+        out = f.read()
+    out = out.split('\n')
+
+    energies  = []
+    run_times = []
+    iter_without_improvement = []
+
+    for line in out[1:]:
+        if not line.startswith('Energy:'):
+            if line.startswith('end inference'):
+                break
+            else:
+                continue
+        values = line.split()
+        energies.append(float(values[1]))
+        run_times.append(int(values[3]))
+        iter_without_improvement.append( int(float(values[5])) )
+
+    if with_iter:
+        return run_times, energies, iter_without_improvement
+    else:
+        return run_times, energies
+
+
+def parse_out_niftyilp(out_path, with_n_violated = False):
+
+    with open(out_path) as f:
+        out = f.read()
+    out = out.split('\n')
+
+    energies  = []
+    run_times = []
+    n_violated = []
+
+    for line in out[1:]:
+        if not line.startswith('Energy:'):
+            if line.startswith('end inference'):
+                break
+            else:
+                continue
+        values = line.split()
+        energies.append(float(values[1]))
+        run_times.append(int(values[3]))
+        n_violated.append( int(float(values[5])) )
+
+    if with_n_violated:
+        return run_times, energies, n_violated
+    else:
+        return run_times, energies
+
+
+def parse_out_mcmp(out_path, return_dual = False):
+
+    with open(out_path) as f:
+        out = f.read()
+    out = out.split('\n')
+
+    primal    = []
+    dual      = []
+    rt_primal = []
+    rt_dual   = []
+    dual_improvements = []
+
+    for ii, line in enumerate(out):
+
+        assert isinstance(line, str), line
+
+        if line.startswith('best triplet'):
+            line = line.split()
+            dual_improvements.append(line[-1])
+            continue
+
+        if not line.startswith('iteration'):
+            continue
+
+        line = line.split()
+        iter_num    = int(line[2][:-1])
+        lower_bound = float(line[6][:-1])
+        run_time   = float(line[-1][:-1])
+
+        rt_dual.append(run_time)
+        dual.append(lower_bound)
+
+        if line[7] == 'upper':
+            val = line[10][:-1] if not line[10] == 'inf,' else '0'
+            primal.append(
+                float( val )
+            )
+            rt_primal.append(run_time)
+
+    # postprocess the primals for a better plot and drop primals that have 0 value
+    primal = np.array(primal)
+    rt_primal = np.array(rt_primal)
+    mask = primal != 0
+    primal = primal[mask]
+    rt_primal = rt_primal[mask]
+
+    if return_dual:
+        return rt_primal, primal, rt_dual, dual, dual_improvements
+    else:
+        return rt_primal, primal
